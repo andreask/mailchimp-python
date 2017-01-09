e377469b9c993ed6c83480f6aba81300e365d967
Andréas Kühne <andreas@kuhne.se>
2017-01-09 16:36:03 +0100

    Fixed creation of new members

--- mailchimp/objects/mc_member.py
+++ mailchimp/objects/mc_member.py
@@ -23,9 +23,9 @@ class MCMember(BaseObject):
         self.id = json_data.get('id')
         self.email_address = json_data.get('email_address')
         self.unique_email_id = json_data.get('unique_email_id')
-        self.email_type = json_data.get('email_type')
-        self.status = json_data.get('status')
-        self.status_if_new = json_data.get('status_if_new')
+        self.email_type = json_data.get('email_type', 'html')
+        self.status = json_data.get('status', 'subscribed')
+        self.status_if_new = json_data.get('status_if_new', 'subscribed')
         self.merge_fields = json_data.get('merge_fields')
         self.interests = json_data.get('interests')
         self.stats = json_data.get('stats')
@@ -38,7 +38,7 @@ class MCMember(BaseObject):
         self.member_rating = json_data.get('member_rating')
         self.last_changed = self._parse_date(json_data.get('last_changed')) if json_data.get('last_changed') else None
         self.language = json_data.get('language')
-        self.vip = json_data.get('vip')
+        self.vip = json_data.get('vip', False)
         self.email_client = json_data.get('email_client')
         self.location = MCLocation(json_data.get('location'))
         self.list_id = json_data.get('list_id')
@@ -48,17 +48,21 @@ class MCMember(BaseObject):
         return_value = {
             'email_type': self.email_type,
             'status': self.status,
-            'merge_fields': self.merge_fields,
-            'interests': self.interests,
-            'language': self.language,
-            'vip': self.vip,
-            'location': self.location.to_dict()
+            'vip': self.vip
         }
 
         if not self.id:
             return_value['email_address'] = self.email_address
             return_value['status_if_new'] = self.status_if_new
         else:
+            if self.language:
+                return_value['language'] = self.language
+            if self.merge_fields:
+                return_value['merge_fields'] = self.merge_fields
+            if self.interests:
+                return_value['interests'] = self.interests
+            if self.location.longitude:
+                return_value['location'] = self.location.to_dict()
             return_value['id'] = self.id
 
         return return_value
@@ -72,7 +76,14 @@ class MCMember(BaseObject):
 
         :return: the url for the list
         """
-        return cls.item_url.replace("{list_id}", list_id)
+        try:
+            return cls.item_url.replace("{list_id}", list_id)
+        except TypeError as e:
+            if list_id:
+                logger.error("Unable to get list url for list_id: %s " % list_id)
+            else:
+                logger.error("Unable to get list url for NoneType list_id")
+            raise e
 
     @classmethod
     def get(cls, list_id, member_id):
@@ -127,9 +138,11 @@ class MCMember(BaseObject):
             logger.error("Unable to delete member from list")
             raise e
 
-    def save(self):
+    def save(self, list_id=None):
         """
         Saves the current member to the list
+
+        :param list_id: the id of the list to save to - if not specified, the members list_id will be used
 
         :return: True if successful
         """
@@ -141,7 +154,10 @@ class MCMember(BaseObject):
             hash_value = md.hexdigest()
 
         try:
-            response = Request.put("%s/%s" % (MCMember.get_list_url(self.list_id), hash_value),
+            if not list_id:
+                list_id = self.list_id
+
+            response = Request.put("%s/%s" % (MCMember.get_list_url(list_id), hash_value),
                                    self.to_dict())
             self._update(response.json())
             return True
